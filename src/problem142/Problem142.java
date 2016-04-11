33b5acae91f27ea5fccb1ad537e0ca973a656f1e
Bill Nicholson <nicholdw@ucmail.uc.edu>
2016-04-11 06:03:16 -0400

    Pretty much Brute Force

--- src/problem142/Problem142.java
+++ src/problem142/Problem142.java
@@ -9,9 +9,32 @@ public class Problem142 {
 	
 	public static int solve() {
 		int result = 0;
-		int x = 1, y = 1, b = 1;
+		int limit = 1006193;
+		int x = 1, y = 1, z = 1;
 		
+		for (x = 3; x < limit; x++) {			
+			for (y = 2; y < x; y++) {
+				if ((x > y) && isSquare(x + y) && isSquare(x - y) ) {	
+					for (z = 1; z < y; z++) {
+						if (y > z) {
+							if (isSquare(x + z)	&&
+								isSquare(x - z)	&&
+								isSquare(y + z)	&&
+								isSquare(y - z)) {
+								System.out.println("x = " + x + " y = " + y + " z = " + z + " x + y + z = " + x + y + z);
+							}
+						}
+					}
+				}
+			}
+		}
 		
+		return x + y + z;
+	}
+	private static boolean isSquare(double num){
+		boolean result = false;
+		int x = (int)Math.sqrt(num);
+		if (x * x == num) result = true;
 		
 		return result;
 	}
