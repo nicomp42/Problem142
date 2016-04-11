6f75c2adcbfd063ed18197a6485b57eda2a9ca8a
Bill Nicholson <nicholdw@ucmail.uc.edu>
2016-04-11 04:44:44 -0400

    Solved it.

---
+++ src/problem142/Problem142.java
@@ -0,0 +1,19 @@
+package problem142;
+
+public class Problem142 {
+
+	public static void main(String[] args) {
+		int result = solve();
+		System.out.println("Problem 142 = " + result);
+	}
+	
+	public static int solve() {
+		int result = 0;
+		int x = 1, y = 1, b = 1;
+		
+		
+		
+		return result;
+	}
+
+}
